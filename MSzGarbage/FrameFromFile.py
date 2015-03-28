87495133f8d21d8b65137d3c5b601b61124e0c34
root <maciej.szaflik@gmail.com>
2015-03-28 19:25:09 +0100

    Read from file with using av

---
+++ MSzGarbage/FrameFromFile.py
@@ -0,0 +1,30 @@
+__author__ = 'maciej'
+import av
+import Image
+import StringIO
+
+
+class FrameReader(object):
+    def __init__(self, pathToFile=""):
+        self.pathToFile = pathToFile
+        self.container = av.open(pathToFile).demux()
+
+
+    def get_frame(self):
+        try:
+            frames = self.container.next().decode()
+        except:
+            self.container = av.open(self.pathToFile).demux()
+            frames = self.container.next().decode()
+
+        for frame in frames:
+            if frame != None:
+                img = frame.to_nd_array()
+                im = Image.fromarray(img)
+                buf = StringIO.StringIO()
+                im.save(buf, format='JPEG')
+                jpeg = buf.getvalue()
+                return jpeg
+
+
+
