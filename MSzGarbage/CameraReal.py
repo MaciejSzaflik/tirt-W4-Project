87ef34d5623b1dc77becc86b9af70120c31b712a
Maciej Szaflik <maciej@maciej-EasyNote-TSX66HR.(none)>
2015-03-28 15:26:58 +0100

    Stream from file

--- MSzGarbage/CameraReal.py
+++ MSzGarbage/CameraReal.py
@@ -3,15 +3,28 @@ import cv2
 import Image
 import StringIO
 
+
 class CameraReal(object):
-    def __init__(self):
-         self.cam = cv2.VideoCapture(0)
+    def __init__(self, fromFile=False, pathToFile=""):
+        self.fromFile = fromFile
+        self.pathToFile = pathToFile
+        if self.fromFile:
+            self.videoFile = cv2.VideoCapture(pathToFile)
+        else:
+            self.cam = cv2.VideoCapture(0)
 
     def get_frame(self):
-        img = self.cam.read()[1]
-        im = Image.fromarray(img)
+        if self.fromFile:
+            flag, img = self.videoFile.read()
+            if flag == 0:
+                self.videoFile.set(cv2.cv.CV_CAP_PROP_POS_FRAMES,0)
+                _,img= self.videoFile.read()
+        else :
+            _,img = self.cam.read()[1]
 
-        buf= StringIO.StringIO()
-        im.save(buf, format= 'JPEG')
-        jpeg= buf.getvalue()
+        im = Image.fromarray(img)
+        buf = StringIO.StringIO()
+        im.save(buf, format='JPEG')
+        jpeg = buf.getvalue()
+        self.lastImage = jpeg
         return jpeg
