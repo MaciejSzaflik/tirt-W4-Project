daeacd76771a77787a427d1f025dacbb96dea3c2
kozakerro <kozakerro@github.com>
2015-04-28 23:16:19 +0200

    Merged wire and unpacker service (redundant packets issue), support for EOFError

--- program/main_sniffer/comss/wire_service.py
+++ program/main_sniffer/comss/wire_service.py
@@ -8,17 +8,7 @@ import socket, sys
 from struct import *
 import threading
 
-class myThread (threading.Thread):
-    def __init__(self, threadID, name, mainObject):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.mainObject = mainObject
-    def run(self):
-        print "alfa 0.2 testing"
-        while self.mainObject.running == 1:
-            self.mainObject.readPacketFromSocket()
-            self.mainObject.wire_output.send(self.mainObject.packet)
+import signal
 
 class WireService(Service):
 
@@ -45,6 +35,8 @@ class WireService(Service):
     def run(self):
         self.wire_output = self.get_output("wireOutput")
         print "Wire service started."
+        while self.running == 1:
+            self.readPacketFromSocket()
 
     def createSocketConnection(self):
         if self.mainSocket == 0:
@@ -53,17 +45,90 @@ class WireService(Service):
             except socket.error, msg:
                 print 'Socket could not be created. Error Code: ' + str(msg[0]) + ' Message ' + msg[1]
                 sys.exit()
-            try:
-                thread1 = myThread(1, "alfa", self)
-                thread1.start()
-            except:
-                print "Error: unable to start thread"
+            #try:
+                #thread1 = myThread(1, "alfa", self)
+                #thread1.start()
+            #except:
+            #    print "Error: unable to start thread"
         else:
             print("pff")
     
     def readPacketFromSocket(self):
-        self.packet = self.mainSocket.recvfrom(16384)#65535)
-        self.nextPCK = self.nextPCK + 1
+    
+        packet = self.mainSocket.recvfrom(16384)#65535)
+        # print packet
+        packetData = self.decipherWhatIsInside(packet)
+        if packetData and (packetData['source']['port'] < 10070 or packetData['source']['port'] > 10080):
+            self.wire_output.send(packet)
+        
+    def eth_addr(self,a):
+        b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+        return b
+
+    def decipherWhatIsInside(self, packet):
+        #parse ethernet header
+        packet = packet[0]
+        eth_length = 14
+
+        eth_header = packet[:eth_length]
+        eth = unpack('!6s6sH' , eth_header)
+        eth_protocol = socket.ntohs(eth[2])
+
+        #Parse IP packets, IP Protocol number = 8
+        if eth_protocol == 8 :
+            #Parse IP header
+            #take first 20 characters for the ip header
+            ip_header = packet[eth_length:20+eth_length]
+
+            #now unpack them :)
+            iph = unpack('!BBHHHBBH4s4s' , ip_header)
+
+            version_ihl = iph[0]
+            version = version_ihl >> 4
+            ihl = version_ihl & 0xF
+
+            iph_length = ihl * 4
+
+            ttl = iph[5]
+            protocol = iph[6]
+            s_addr = socket.inet_ntoa(iph[8]);
+            d_addr = socket.inet_ntoa(iph[9]);
+            if protocol == 6 :
+                t = iph_length + eth_length
+                tcp_header = packet[t:t+20]
+
+                #now unpack them :)
+                tcph = unpack('!HHLLBBHHH' , tcp_header)
+
+                source_port = tcph[0]
+                dest_port = tcph[1]
+                sequence = tcph[2]
+                acknowledgement = tcph[3]
+                doff_reserved = tcph[4]
+                tcph_length = doff_reserved >> 4
+
+                h_size = eth_length + iph_length + tcph_length * 4
+
+                #get data from the packet
+                body = packet[h_size:]
+
+                data = {}
+                data['eth_protocol'] = eth_protocol
+                data['version'] = version
+                data['ihl'] = ihl
+                data['source'] = {}
+                data['source']['address'] = s_addr
+                data['source']['port'] = source_port
+                data['target'] = {}
+                data['target']['address'] = d_addr
+                data['target']['port'] = dest_port
+                data['sequence'] = sequence
+                data['acknowledgement'] = acknowledgement
+                data['doff_reserved'] = doff_reserved
+                data['tcph_length'] = tcph_length
+                data['h_size'] = h_size
+
+                return data
 
 if __name__=="__main__":
     sc = ServiceController(WireService, "wire_service.json")
