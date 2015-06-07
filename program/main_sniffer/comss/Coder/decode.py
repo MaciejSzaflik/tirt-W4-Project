e01007580fcf52e11388d8eee74e86517f8d1c8d
michalvvv <michalvvv@users.noreply.github.com>
2015-06-07 22:21:24 +0200

    usunięcie komentarzy w decode.py

--- program/main_sniffer/comss/Coder/decode.py
+++ program/main_sniffer/comss/Coder/decode.py
@@ -5,15 +5,10 @@ import json
 
 def decode(data):
     try:
-        #print imgarray[0]
         len1 = int(data[0])
-        #print str(len1)
         len2 = int(data[2:(int(data[0]) + 2)])
-        #print str(len2) + " \n"
         packetData = data[3 + len1:(3 + len1 + len2)]
-        #print packetData
         imgarray = data[(4 + len1 + len2):]
-        #print "!!!" + imgarray + "!!!"
         try:
             packetData = json.loads(packetData)
 
